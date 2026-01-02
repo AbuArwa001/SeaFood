3549982ca648b482922b024620600957678cc499
AbuArwa001 <khalfanathman12@yahoo.com>
2026-01-02 11:53:13 +0300

    Refactor serializers to include CurrencySerializer and enhance ExchangeRateSerializer with additional fields for currency codes

--- exchangerates/serializers.py
+++ exchangerates/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import ExchangeRate
+from currencies.models import Currency
+
+class CurrencySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Currency
+        fields = [
+            'id',
+            'code',
+            'name',
+            'symbol',
+        ]
 
 class ExchangeRateSerializer(serializers.ModelSerializer):
+    # from_currency = CurrencySerializer(read_only=True)
+    from_curr = serializers.CharField(source='from_currency.code', read_only=True)
+    to_curr = serializers.CharField(source='to_currency.code', read_only=True)
+    # to_currency = CurrencySerializer(read_only=True)
     class Meta:
         model = ExchangeRate
         fields = [
@@ -10,5 +25,7 @@ class ExchangeRateSerializer(serializers.ModelSerializer):
             'to_currency',
             'rate',
             'rate_date',
+            'from_curr',
+            'to_curr',
             'created_at',
         ]
