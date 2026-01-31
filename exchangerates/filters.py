6f804e6399a9ba6b7ec1119139903d176cfa0162
AbuArwa001 <khalfanathman12@yahoo.com>
2026-01-31 16:39:21 +0300

    feat: Introduce a script for resetting user passwords and extend exchange rate filtering with multi-currency support.

--- exchangerates/filters.py
+++ exchangerates/filters.py
@@ -9,7 +9,13 @@ class ExchangeRateFilter(filters.FilterSet):
     from_currency__code = filters.CharFilter(field_name='from_currency__code', lookup_expr='iexact')
     to_currency__code = filters.CharFilter(field_name='to_currency__code', lookup_expr='iexact')
     rate_date = filters.DateFromToRangeFilter(field_name='rate_date')
+    currencies = filters.CharFilter(method='filter_currencies')
 
     class Meta:
         model = ExchangeRate
-        fields = ['from_currency__code', 'to_currency__code', 'rate_date']
+        fields = ['from_currency__code', 'to_currency__code', 'rate_date', 'currencies']
+
+    def filter_currencies(self, queryset, name, value):
+        if value:
+            currency_codes = [code.strip() for code in value.split(',')]
+            return queryset.filter(from_currency__code__in=currency_codes)
