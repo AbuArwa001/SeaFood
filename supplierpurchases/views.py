a52b1ef6235eb819104614bde0938c2fde63dae4
AbuArwa001 <khalfanathman12@yahoo.com>
2026-01-31 12:36:54 +0300

    fix: Change `SupplierPurchaseViewSet` queryset from `User` to `SupplierPurchase`.

--- supplierpurchases/views.py
+++ supplierpurchases/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
+from supplierpurchases.models import SupplierPurchase
 from .serializers import SupplierPurchaseSerializer
-from users.models import User
+
 
 class SupplierPurchaseViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = SupplierPurchase.objects.all()
     serializer_class = SupplierPurchaseSerializer
