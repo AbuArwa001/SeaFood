3ab878b77ec3fbbf85c65469f49dcb6a49d3bde4
AbuArwa001 <khalfanathman12@yahoo.com>
2026-02-20 17:42:37 +0300

    fix: show categories for products

--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
 from .models import Product
+from productcategories.serializers import ProductCategorySerializer
+from unitofmeasures.serializers import UnitOfMeasureSerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category_name = serializers.ReadOnlyField(source='category.name')
-    unit_name = serializers.ReadOnlyField(source='unit.name')
+    unit_name = serializers.ReadOnlyField(source='unit.code')
 
     class Meta:
         model = Product
@@ -18,3 +20,9 @@ class ProductSerializer(serializers.ModelSerializer):
             'is_active',
             'created_at',
         )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category'] = ProductCategorySerializer(instance.category).data
+        representation['unit'] = UnitOfMeasureSerializer(instance.unit).data
+        return representation
