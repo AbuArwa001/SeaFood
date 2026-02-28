f66679e2539cd56bca6a0fd918cd411f0eb57c4a
AbuArwa001 <khalfanathman12@yahoo.com>
2026-02-28 17:49:56 +0300

    Refactor `ExchangeRateSerializer` to use `_detail` suffixes for nested currency serializers and update the `Currency` model import path.

--- exchangerates/serializers.py
+++ exchangerates/serializers.py
@@ -1,17 +1,10 @@
 from rest_framework import serializers
-from .models import ExchangeRate
-from currencies.models import Currency
+from .models import ExchangeRate, Currency, CurrencyMargin
 
 class CurrencySerializer(serializers.ModelSerializer):
     class Meta:
         model = Currency
-        fields = [
-            'id',
-            'code',
-            'name',
-        ]
-
-from .models import CurrencyMargin
+        fields = ['id', 'code', 'name']
 
 class CurrencyMarginSerializer(serializers.ModelSerializer):
     from_currency_detail = CurrencySerializer(source='from_currency', read_only=True)
@@ -32,16 +25,20 @@ class CurrencyMarginSerializer(serializers.ModelSerializer):
         ]
 
 class ExchangeRateSerializer(serializers.ModelSerializer):
-    from_currency = CurrencySerializer(read_only=True)
+    from_currency_detail = CurrencySerializer(source='from_currency', read_only=True)
+    to_currency_detail = CurrencySerializer(source='to_currency', read_only=True)
+    
     from_curr = serializers.CharField(source='from_currency.code', read_only=True)
     to_curr = serializers.CharField(source='to_currency.code', read_only=True)
-    to_currency = CurrencySerializer(read_only=True)
+
     class Meta:
         model = ExchangeRate
         fields = [
             'id',
             'from_currency',
             'to_currency',
+            'from_currency_detail',
+            'to_currency_detail',
             'rate',
             'rate_date',
             'from_curr',
