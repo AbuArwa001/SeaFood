51b1c83a306c668efed53cf8d8813de02d2d2ecc
AbuArwa001 <khalfanathman12@yahoo.com>
2026-04-05 12:04:31 +0300

    feat: add root URL redirect to the API v1 endpoint

--- seafood/urls.py
+++ seafood/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 from seafood import root_app
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -30,4 +31,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include(api_v1_patterns)),
+    path('', lambda request: redirect('/api/v1/', permanent=False)),
 ]
